b684f10d4930f54db210fa9858a6710b2fd36a73
robin <robinv1507@gmail.com>
2019-07-13 18:03:08 +0530

    Java Script programs

---
+++ javaScript/event.js
@@ -0,0 +1,13 @@
+
+let button=document.getElementById('myButton');
+// button.onclick=
+function createNewElement(){
+
+    document.getElementById('div1').innerHTML='<a href="https://www.google.com">Google <a/>';
+};
+
+
+window.onload=function(){
+
+    document.getElementById('mybutton').textContent='Dont click';
+}
