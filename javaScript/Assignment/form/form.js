0ca9fa216e59ad3f7c26631442f3e7efcbd1fca1
robin <robinv1507@gmail.com>
2019-07-16 18:38:45 +0530

    javaScript Assignment

--- javaScript/Assignment/form/form.js
+++ javaScript/Assignment/form/form.js
@@ -1,7 +1,7 @@
 //login validation
 function validateForm(){
     let myEmail= document.forms[0].email.value;
-    let myPassword= document.forms[0].password.value;
+    let myPassword= document.forms[0].cPassword.value;
     let myConformPassword= document.forms[0].password.value;
     console.log(myEmail);
     console.log(myPassword);
@@ -10,13 +10,34 @@ function validateForm(){
 
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myEmail) && 
          myEmail.length>6&&
-         myPassword>6&&
+         myPassword.length>6&&
          myConformPassword.length>6&&
-         myPassword==myConformPassword)
+         myConformPassword===myPassword)
     {
         document.getElementById('submitButton').disabled=false;
     }else{
         document.getElementById('submitButton').disabled=true;
     }
+}
+//show password 
+function showPassword(){
     
+       if(  document.getElementById('password1').type=="password"){
+  document.getElementById('password1').type="text";
+       }else{
+        document.getElementById('password1').type="password";
+
+       }
+ 
+}
+
+function showcPassword(){
+    
+    if(  document.getElementById('password2').type=="password"){
+document.getElementById('password2').type="text";
+    }else{
+     document.getElementById('password2').type="password";
+
+    }
+
 }
