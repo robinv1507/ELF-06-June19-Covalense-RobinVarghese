05b5f4c77e2b4c98123c2301113f64a1dd331758
robin <robinv1507@gmail.com>
2019-07-17 19:57:10 +0530

    JavaScript  Assignments

--- javaScript/Assignment/form/form.js
+++ javaScript/Assignment/form/form.js
@@ -1,13 +1,25 @@
 //login validation
+
+
 function validateForm(){
-    let myEmail= document.forms[0].email.value;
-    let myPassword= document.forms[0].cPassword.value;
-    let myConformPassword= document.forms[0].password.value;
-    console.log(myEmail);
+    var myEmail= document.forms[0].email.value;
+var myPassword= document.forms[0].cPassword.value;
+var myConformPassword= document.forms[0].password.value;
+localStorage.setItem("email", myEmail);
+localStorage.setItem("password", myPassword);
+localStorage.setItem("cpassword", myConformPassword);
+
+//console.log("lstroer"+localStorage.getItem("email"));
+
+// function tableData(){
+//     return myEmail;
+// }
+//    
+ console.log(myEmail);
     console.log(myPassword);
     console.log(myConformPassword);
 
-
+      //validation  for all input
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myEmail) && 
          myEmail.length>6&&
          myPassword.length>6&&
@@ -15,9 +27,36 @@ function validateForm(){
          myConformPassword===myPassword)
     {
         document.getElementById('submitButton').disabled=false;
+
     }else{
         document.getElementById('submitButton').disabled=true;
     }
+    //validation message for email
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myEmail) && 
+         myEmail.length>6)
+    {
+        document.getElementById('emailMessage').style.visibility='hidden';
+        
+    }else{
+        document.getElementById('emailMessage').style.visibility='visible';
+    }
+    //validation message for password
+    if ( myPassword.length>6)
+    {
+        document.getElementById('passwordMessage').style.visibility='hidden';
+        
+    }else{
+        document.getElementById('passwordMessage').style.visibility='visible';
+    }
+
+     //validation message for conform password
+     if (myConformPassword===myPassword)
+     {
+         document.getElementById('cpasswordMessage').style.visibility='hidden';
+         
+     }else{
+         document.getElementById('cpasswordMessage').style.visibility='visible';
+     }
 }
 //show password 
 function showPassword(){
@@ -30,7 +69,7 @@ function showPassword(){
        }
  
 }
-
+  //show password
 function showcPassword(){
     
     if(  document.getElementById('password2').type=="password"){
@@ -39,5 +78,56 @@ document.getElementById('password2').type="text";
      document.getElementById('password2').type="password";
 
     }
+
+}
+
+
+//sign in form data table 
+
+function showTable(){
+    var theadCount=1;
+    console.log(theadCount);
+
+var myemail=localStorage.getItem("email");
+var mypswd=localStorage.getItem("password");
+var mycpswd=localStorage.getItem("cpassword");
+
+let signInData = [
+    { Email: myemail,Password: mypswd,Conform_Password: mycpswd },
+   
+   
+  ];
+
+  function generateTableHead(table, data) {
+   
+    let thead = table.createTHead();
+    let row = thead.insertRow();
+    for (let key of data) {
+      let th = document.createElement("th");
+      let text = document.createTextNode(key);
+      th.appendChild(text);
+      row.appendChild(th);
+    }
+  }
+  function generateTable(table, data) {
+    for (let element of data) {
+      let row = table.insertRow();
+      for (key in element) {
+        let cell = row.insertCell();
+        let text = document.createTextNode(element[key]);
+        cell.appendChild(text);
+      }
+    }
+  }
+  let table = document.querySelector("table");
+  let data = Object.keys(signInData[0]);
+  
+  if(theadCount===1){
+      console.log("if")
+      theadCount=2;
+        generateTableHead(table, data);
+       
+  }
+  generateTable(table, signInData);
 
 }
